788ec9137c5b8987809a8609eee552a6f49dac66
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-02-25 01:05:05 +0100

    Added the social dep

--- home/settings/base.py
+++ home/settings/base.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'rest_auth.registration',
-    'allauth.socialaccount',
+    'widget_tweaks',
 
 ]
 SITE_ID = 1
--- home/settings/dev.py
+++ home/settings/dev.py
@@ -1,19 +1,19 @@
-'''Use this for development'''
+# '''Use this for development'''
 
-from .base import *
+# from .base import *
 
-ALLOWED_HOSTS += ['127.0.0.1']
-DEBUG = True
+# ALLOWED_HOSTS += ['127.0.0.1']
+# DEBUG = True
 
-WSGI_APPLICATION = 'home.wsgi.dev.application'
+# WSGI_APPLICATION = 'home.wsgi.dev.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-)
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:3000',
+# )
